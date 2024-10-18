3b3d63903e900f0012d8281c805562249c829140
Henry Mao <122509674+henhenmao@users.noreply.github.com>
2024-10-17 22:36:17 -0700

    Calculator can perform basic tasks (not very well) - made the operators and numbers work together well enough - you can type 1+1 and then equals and then get 2 on the display - will be issues for multiple operations and other things - but it works a little bit

--- j.js
+++ j.js
@@ -1,21 +1,32 @@
-let n1;
-let n2;
+let n1 = "";
+let n2 = "";
+let pressedOperation = false; // indicates beginning of n2
 let operation;
 let result;
+let digit;
 
 // all selectors for each of the the buttons
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const display = document.querySelector("#display");
 const reset = document.querySelector("#reset");
+const equals = document.querySelector("#equals");
 
 // making the numbers work
 numbers.forEach((n) => {
     n.addEventListener("click", e => {
-        display.textContent += e.target.innerHTML;
+        digit = e.target.innerHTML
+        display.textContent += digit;
+        if (!pressedOperation) {
+            n1 += digit;
+            console.log(`n1: ${n1}`);
+        } else {
+            n2 += digit;
+            console.log(`n2: ${n2}`);
+        }
     });
 });
-// learning how to use target and innerhtml 
+// learning how to use target and innerhtml and converting to Numbers
 // numbers.addEventListener("click", e => {
 //     const target = e.target;
 //     const value = Number(target.innerHTML);
@@ -25,34 +36,46 @@ numbers.forEach((n) => {
 
 
 // making the reset button reset things
-reset.addEventListener("click", () => {
-    console.log("hellow rodl!");
-    display.textContent = "";
-    n1 = null;
-    n2 = null;
-});
+reset.addEventListener("click", resetValues);
 
 
 // making operator functions work kind of
-console.log(operators);
 operators.forEach((n) => {
-    console.log(n.textContent);
     n.addEventListener("click", () => {
         display.textContent += n.textContent;
+        operation = n.textContent; // setting tyoe of operation to perform
+        pressedOperation = true; // allows for n2 to take over
     });
 });
 
 
+//m aking the equals buttons work properly
+equals.addEventListener("click", () => {
+    result = operate(Number(n1), Number(n2), operation);
+    console.log(result);
+    display.textContent = result;
+});
+
+
+function resetValues() {
+    display.textContent = "";
+    n1 = "";
+    n2 = "";
+    operation = null;
+    pressedOperation = false;
+}
+
+
 // operation functions
 function operate(n1, n2, operation) {
-    if (operation == "add") {
-        alert("add")
-    } else if (operation == "subtract") {
-        alert("subtrack")
-    } else if (operation == "multiply") {
-        alert("multiply  burrton")
-    } else if (operation == "divide") {
-        alert("divide")
+    if (operation == "+") {
+        return n1 + n2;
+    } else if (operation == "-") {
+        return n1 - n2;
+    } else if (operation == "*") {
+        return n1 * n2;
+    } else if (operation == "÷") {
+        return n1 / n2;
     } else {
         alert("operation did not work")
     }
