7216ae9c90f61ee07a62b2875968b3079c24ee32
Henry Mao <122509674+henhenmao@users.noreply.github.com>
2024-10-19 15:46:51 -0700

    finished calculator - separated each button into different functions to make it easier to read - made numbers, operators, equals, reset, sign, decimal, and delete button work

--- j.js
+++ j.js
@@ -4,6 +4,7 @@ let first = true; // used as a selector - true = n1, false = n2
 let operation = "";
 let result;
 let digit;
+let justEquals = false;
 
 
 const display = document.querySelector("#display")
@@ -14,46 +15,57 @@ buttons.forEach(button => {
     button.addEventListener("click", () => {
         if (button.classList.contains("number")) {
             numberClick(button.textContent);
-        }
-        if (button.classList.contains("operator")) {
+        } else if (button.classList.contains("operator")) {
             operatorClick(button.textContent);
-        }
-        if (button.classList.contains("equals")) {
+        } else if (button.classList.contains("equals")) {
             equalsClick();
-        }
-        if (button.classList.contains("reset")) {
+        } else if (button.classList.contains("reset")) {
             resetClick();
+        } else if (button.classList.contains("dot")) {
+            dotClick();
+        } else if (button.classList.contains("sign")) {
+            signClick();
+        } else if (button.classList.contains("delete")) {
+            clickDelete();
         }
     })
 });
 
 function numberClick(n) {
+    if (justEquals) { // start new equation if equals has just been pressed
+        resetClick();
+        justEquals = false;
+    }
     if (!operation) {
         n1 += n;
-        console.log(`n1: ${n1}`);
     } else {
         n2 += n;
-        console.log(`n2: ${n2}`);
     }
     addDisplay(n);
 }
 
 
 function operatorClick(operator) {
-    operation = operator;
-    first = false;
-    addDisplay(operation);
+    if (n1 != "" && n2 != "" && operation != "") { // chaining operators kind of
+        equalsClick();
+    }
+    if (!operation) {
+        operation = operator;
+        first = false;
+        addDisplay(operation);
+        justEquals = false;
+    }
 }
 
 function equalsClick() {
-    if (!n1 || !n2 || !operation) { }
+    if (n1 === "" || n2 === "" || operation === "") {}
     else {
         result = operate(Number(n1), Number(n2), operation);
         n1 = result;
         n2 = "";
         operation = "";
         setDisplay(result);
-        first = true;
+        justEquals = true;
     }
 }
 
@@ -66,8 +78,59 @@ function resetClick() {
 }
 
 
+function dotClick() {
+    if (first) {
+        if (!n1.includes(".")) {
+            n1 += ".";
+            addDisplay(".");
+        }
+    } else {
+        if (!n2.includes(".")) {
+            n2 += ".";
+            addDisplay(".");
+        }
+    }
+}
+
+function signClick() {
+    if (justEquals) {
+        resetClick();
+        justEquals = false;
+        return;
+    }
+    if (first) {
+        if (n1 == "") {
+            n1 += "-";
+            addDisplay("-")
+        }
+    } else {
+        if (operation && n2 == "") {
+            n2 += "-";
+            addDisplay("-");
+        }
+    }
+}
+
+function clickDelete() {
+    if (justEquals) {
+        resetClick();
+        justEquals = false;
+        return;
+    }
+    if (!operation && n2 == "") { // currently selecting n1
+        n1 = n1.substring(0, n1.length - 1);
+    } else if (n2 == "") { // currently selecting the operator
+        operation = "";
+    } else {
+        n2 = n2.substring(0, n2.length - 1);
+    }
+    deleteDisplay();
+
+}
+
+
 function operate(n1, n2, operation) {
-    if (operation == "÷" && n2 == 0) { // closes the tab if divudes by zero
+    if (operation == "÷" && n2 == 0) {
         window.close();
     }
     if (operation == "+") {
@@ -89,6 +152,10 @@ function setDisplay(s) {
 
 function addDisplay(s) {
     display.textContent += s;
+}
+
+function deleteDisplay() {
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
 }
 
 function clearDisplay() {
